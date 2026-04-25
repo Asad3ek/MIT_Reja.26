654e6c0f85b450b0de7cebc2e261c7da4f294a5a
Asad3ek <asadbeksaydullayev2911@gmail.com>
2026-04-25 13:11:06 +0900

    feat: MIT task-A | Callback && Asychronus functions

---
+++ train.js
@@ -0,0 +1,108 @@
+//Callback function — bu boshqa function ichiga argument sifatida beriladigan va keyinroq chaqiriladigan function
+
+//console.log("aadi family clothing shop");
+const check = [ 
+    "Good shoes improve your whole look. To make more quality your look with aadi Please go Men's clothes page >>", //men
+    "Use neutral colors for a clean look. To feel more confident PLease visit aadi woman's page >>", //woman
+    "Choose basic colors like black, white, and blue. Please visit Boy's page >>", // Boys 
+    "Wear bright and fun colors. Please visit Girl's page >>"  //Girls
+]
+//define
+function choosingType(a, callback){
+    if (typeof a !== 'string') callback("Please, insert your gender type", null)
+        else if(a === "Men") setTimeout(function() {
+            callback(null, check[0]);
+    }, 3000)
+        else if(a === "Woman") callback(null, check[1]);
+        else if(a === "Boy") callback(null, check[2]);
+        else if(a === "Girl") callback(null, check[3]);
+
+}
+
+//call 
+// console.log("first come first service")
+// choosingType("Men", (err, data) =>{
+//     if(err) console.log('ERROR:', err);
+//    else {
+//        console.log('aadi:',data);
+//    }
+// })
+// console.log("first come first service 222222")
+
+//Asncr functionlar 
+//console.log("Jack ma maslahatlari")
+const list = [
+    "Yaxshi talaba bo'ling", //0-20
+    "O'zingizga investitsiya qiling", //20-30
+    "To'g'ri boshliq tanlang va xato qilishdan qo'rqmang", //30-40
+    "Yoshlarga Investitsiya qiling",//40-50
+    "Yoshlar bilan ko'proq suhbat qiling, maslahat ulalshing",//50-60
+    "Endi dam oling" //60-
+]
+
+async function maslahatBering(a) {
+    if(typeof a !== "number") throw new Error("Please insert number")
+        else if(a <= 20) return(list[0])
+        else if(a >= 20 && a <= 30) return(list[1])
+        else if(a > 30 && a <= 40) return (list[2])
+        else if(a > 40 && a <= 50) return (list[3])
+        else if(a > 50 && a <= 60) return (list[4])
+        else {
+            return new Promise((resolve, reject) =>{
+                setTimeout(() => {
+                    resolve(list[5]);
+                }, 4000);
+            })
+        } 
+    }
+
+//call
+// console.log("1. first colum");
+
+// maslahatBering(55).then((data) => {
+//     console.log("Jac Ma:", data);
+
+// }).catch((err) => {
+//     console.log("ERORR:", err);
+// })
+
+// console.log("3. third colum");
+//async functionlar sync functionlarga ishga tushib bo'lgach ishlaydi. Va shu yo'l bn thred ni band qilmaydi.
+
+//call => asncr & await
+async function run() {
+    let advice = await maslahatBering(65);
+    console.log(advice);
+    advice = await maslahatBering(34);
+    console.log(advice);
+    advice = await maslahatBering(44);
+    console.log(advice);
+}
+//run();
+
+//============M=====================I========================T=========================
+// MITASK-A 
+function countDigit(digit, number) {
+    let count = 0;
+    for(let i = 0; i < number.length; i++){
+        if(number[i] === digit){
+            count++
+        }
+    }
+    return count;
+}
+const result1 = (countDigit("7", "25327954674590459085978636983598457869426879"))
+console.log("Result 1:", result1);
+
+//function 2
+function countLetter(letter, word) {
+    let count = 0;
+    for(let i = 0; i < word.length; i++){
+        if(word[i] === letter){
+            count++
+        }
+    }
+    return count;
+}
+ const result = countLetter("a","Mehanizatsiyalashtirilmaganligidanda");
+ console.log("Result 2:", result);
